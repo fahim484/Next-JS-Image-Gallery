bcef3350827a463754fe0c7c2f056b14a0e79be9
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2025-02-17 11:07:06 +0600

    Enhance home page layout with improved styling and background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <main className="flex min-h-screen items-center justify-center bg-sky-100">
+      <div className="w-full max-w-screen-2xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg">
+        <h1 className="text-4xl font-bold text-green-500">Home Page</h1>
+      </div>
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
