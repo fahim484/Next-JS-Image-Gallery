23df5d31b2176d8026eae68610b73c08c26761a3
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2025-02-20 11:39:19 +0600

    Implement search functionality in Gallery component and update Header to manage search state

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -7,10 +7,35 @@ export const Gallery = () => {
   const params = useSearchParams();
   const category = params.get("category");
   // console.log("🚀 ~ Gallery ~ param:", category);
+  const search = params.get("search");
+  console.log("🚀 ~ Gallery ~ param:", search);
 
   // const images = data.filter((item,) => !category || item.category === category);
   // const images = data.filter((item,) => category ? item.category === category: true);
-  const images = data.filter((item,) => category ? item.category === category: item);
+  // const images = data.filter((item,) => category ? item.category === category: item);
+  // const images = data.filter((item,) => category && item.category === category || search && item.name.toLowerCase().includes(search.toLowerCase()) || item);
+
+  // const images = data.filter((item) => {
+  //   if (category && search) {
+  //     return item.category === category && item.name.toLowerCase().includes(search.toLowerCase());
+  //   } else if (category) {
+  //     return item.category === category;
+  //   } else if (search) {
+  //     return item.name.toLowerCase().includes(search.toLowerCase());
+  //   } else {
+  //     return true;
+  //   }
+  // });
+
+  const images = data.filter(
+    (item) =>
+      (!category || item.category === category) &&
+      (!search || item.name.toLowerCase().includes(search.toLowerCase()))
+  );
+
+  // const images = data.filter((item) =>
+  //   item.name.toLocaleLowerCase().includes(search?.toLocaleLowerCase() || "")
+  // );
 
   return (
     <div className="pt-14 grid grid-cols-3 gap-4">
@@ -18,9 +43,11 @@ export const Gallery = () => {
       {/* {data.map((item) => (
         <Card key={item.id} {...item} />
       ))} */}
-      {images.map((item) => (
-        <Card key={item.id} {...item} />
-      ))}
+      {images.length > 0 ? (
+        images.map((item) => <Card key={item.id} {...item} />)
+      ) : (
+        <h2 className="text-slate-300 font-bold text-center col-span-3 text-3xl pt-5">No images found!</h2>
+      )}
     </div>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { SearchIcon } from "./icons";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const [searchData, setSearchData] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // if (searchData) {
+    //   router.push(`/?search=${searchData}`);      
+    // }
+    router.push(searchData ? `/?search=${searchData}` : "/");
+  }, [searchData, router]);
+  
+
   return (
     <div className="flex justify-between items-center gap-2.5">
       <div className="w-1/3 max-w-xl flex justify-center items-center">
@@ -9,6 +23,8 @@ const Header = () => {
           type="text"
           placeholder="Search..."
           className="inline-block w-full py-2 pr-10 px-4 text-slate-500 bg-slate-100 border-2 border-slate-400 focus:outline-none rounded"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
         />
         <button type="button" className="w-8 text-slate-500 -ml-8">
           <SearchIcon />
